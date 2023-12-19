e4123058696906c677817b26802f9bd35b8195d2
Barrett Flowers <barrettjflowers@gmail.com>
2023-12-18 22:47:35 -0500

    Efficieny refactor

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,33 +1,34 @@
 import fs from "fs";
 
+//npx ts-node ./path_to_file
+//npm i typescript --save-dev
+//npx tsc --init
+
 //read the input and sort out alpha characters
 fs.readFile(
   "input.txt",
   "utf-8",
   (err: NodeJS.ErrnoException | null, data: any) => {
     if (err) throw err;
-    data = data.replace(/[^\d\n-]/g, "");
-    data = data.split("\n");
+    data = data
+      .replace(/[^\d\n-]/g, "")
+      .split("\n")
+      .map(function (element: string) {
+        const firstChar = element.slice(0, 1);
+        const lastChar = element.slice(-1);
 
-    //set input to a map and save first and last character
-    const output: string[] = data.map(function (element: string) {
-      const firstChar = element.slice(0, 1);
-      const lastChar = element.slice(-1);
-
-      return firstChar + lastChar;
-    });
+        return firstChar + lastChar;
+      });
 
     //convert to int and sum
-    function calcSum(): number {
-      const numberArray: number[] = [];
-      const initialVal: number = 0;
-      output.forEach((ele: any) => numberArray.push(+ele));
-      const sum: number = numberArray.reduce(
-        (acc: number, current: number) => acc + current,
-        initialVal
-      );
-      return sum;
-    }
-    console.log("Your answer is: " + calcSum());
+    const numberArray: number[] = [];
+    const initialVal: number = 0;
+    data.forEach((ele: any) => numberArray.push(+ele));
+    const sum: number = data.reduce(
+      (acc: number, current: number) => acc + current,
+      initialVal
+    );
+
+    console.log("Your answer is: " + sum);
   }
 );
